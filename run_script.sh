6b63bb616091a4a154a88e0e32a216a396e2ae6f
Luka Golinar <luka.golinar@gmail.com>
2025-04-03 17:04:16 +0200

    feat: string

--- run_script.sh
+++ run_script.sh
@@ -7,6 +7,6 @@ nohup node crawler.js \
   "0x996eF3cfd6c788618C359Fb538D49281a0b13805" \
   "7248332" \
   "mainnet" \
-  `${START_TIME}` 2>&1 &
+  "${START_TIME}" 2>&1 &
 
 echo "All processes started and running in the background."
