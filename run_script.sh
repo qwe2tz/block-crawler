9c22857fd864922d9b63502d0209378019515509
Luka Golinar <luka.golinar@gmail.com>
2025-04-04 11:12:41 +0200

    feat: vup

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+
 nohup node crawler.js \
   "neuroweb" \
   "0x996eF3cfd6c788618C359Fb538D49281a0b13805" \
@@ -10,6 +12,19 @@ nohup node crawler.js \
   "0x4464A1c89C09a8D6062628D22AF9327485C81DB8" \
   "7248332" \
   "testnet" > neuroweb-tesnet.log 2>&1 &
+
+nohup node crawler.js \
+  "gnosis" \
+  "0x828405dfc287f7d9b9cc0588d036f2b94231e166" \
+  "37724991" \
+  "mainnet" > gnosis-mainnet.log 2>&1 &
+
+
+nohup node crawler.js \
+  "base" \
+  "0xd5ed8eab35536f8c33c38128087441218df65b1c" \
+  "24221990" \
+  "mainnet" > base-mainnet.log 2>&1 &
 
 
 echo "All processes started and running in the background."
