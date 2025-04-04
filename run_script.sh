f206f183ffc876ed0c1b7f0bc5c19eead6bbf45a
Luka Golinar <luka.golinar@gmail.com>
2025-04-04 10:44:22 +0200

    feat: redirect to log

--- run_script.sh
+++ run_script.sh
@@ -3,13 +3,13 @@ nohup node crawler.js \
   "neuroweb" \
   "0x996eF3cfd6c788618C359Fb538D49281a0b13805" \
   "7248332" \
-  "mainnet" 2>&1 &
+  "mainnet" > neuroweb-mainnet.log 2>&1 &
 
 nohup node crawler.js \
   "neuroweb" \
   "0x4464A1c89C09a8D6062628D22AF9327485C81DB8" \
   "7248332" \
-  "testnet"  2>&1 &
+  "testnet" > neuroweb-tesnet.log 2>&1 &
 
 
 echo "All processes started and running in the background."
